787e8d89b31f566727a8d2a151b7ba9d4a18526a
nayoung <yny3533@naver.com>
2024-06-28 15:53:03 +0900

    1일차 수업 내용

--- src/main/java/org/example/basic/BasicApplication.java
+++ src/main/java/org/example/basic/BasicApplication.java
@@ -1,13 +1,29 @@
 package org.example.basic;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BasicApplication {
-
     public static void main(String[] args) {
+
+        Singleton singleton = Singleton.getInstance();
+        Singleton singleton1 = Singleton.getInstance();
+        Singleton singleton2 = Singleton.getInstance();
+
+        System.out.println(singleton);
+        System.out.println("====================================");
+        System.out.println(singleton1);
+        System.out.println("====================================");
+        System.out.println(singleton2);
+
+        /*Singleton singleton3 = new Singleton();
+        System.out.println(singleton3);
+        System.out.println("====================================");
+        Singleton singleton4 = new Singleton();
+        System.out.println(singleton4);*/
+
         SpringApplication.run(BasicApplication.class, args);
     }
-
 }
---
+++ src/main/java/org/example/basic/UserRepositoryInterface.java
@@ -0,0 +1,8 @@
+package org.example.basic;
+
+public interface UserRepositoryInterface {
+
+    void save(User user);
+
+    void delete(User user);
+}
