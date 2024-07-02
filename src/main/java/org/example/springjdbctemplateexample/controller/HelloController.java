400c9e9778e2d18bcd91b9936bc3b9e749866ae4
nayoung <yny3533@naver.com>
2024-07-02 17:32:06 +0900

    subtree 커밋테스트

--- src/main/java/org/example/springjdbctemplateexample/controller/HelloController.java
+++ src/main/java/org/example/springjdbctemplateexample/controller/HelloController.java
@@ -1,8 +1,5 @@
 package org.example.springjdbctemplateexample.controller;
 
-import org.example.springjdbctemplateexample.repository.StudentRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
